c1b037528c56f5f04e75732849c5f62a97d81534
Hetti219 <sathikahettiarachchi219@gmail.com>
2024-07-10 07:10:51 +0530

    Update login_page.dart

--- lib/authentication/login_page.dart
+++ lib/authentication/login_page.dart
@@ -8,10 +8,63 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _email = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
-      appBar: AppBar(title: Text('Login'),),
+      appBar: AppBar(
+        title: Text('Login', style: theme.textTheme.headlineLarge),
+        centerTitle: true,
+        backgroundColor: theme.colorScheme.primary,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(
+                  height: 20,
+                ),
+                Image.asset(
+                  'assets/images/Gemini_Generated_Image_exos46exos46exos.jpeg',
+                  height: 200,
+                  width: 200,
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                      TextField(
+                      controller: _email,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          labelText: 'Enter User Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ))),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                        'Reset Account Device?',
+                        style: theme.textTheme.labelSmall,
+                  ),
+                )
+              ]),
+        )
+        ],
+      ),
+    ),)
     );
   }
 }
