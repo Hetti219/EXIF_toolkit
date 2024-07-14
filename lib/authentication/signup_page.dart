cf99a652bc95a417e036460066e2e33a96435503
Hetti219 <sathikahettiarachchi219@gmail.com>
2024-07-14 16:36:04 +0530

    Update signup_page.dart

--- lib/authentication/signup_page.dart
+++ lib/authentication/signup_page.dart
@@ -1,4 +1,7 @@
+import 'package:exif_toolkit/authentication/auth_service.dart';
+import 'package:exif_toolkit/authentication/input_validator.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -8,8 +11,72 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final _auth = AuthService();
+
+  //Controllers
+  final _email = TextEditingController();
+
+  //Error messages
+  String? _emailError;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _email.addListener(_validateEmail);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _email.dispose();
+  }
+
+  //Email Validator
+  void _validateEmail() {
+    setState(() {
+      _emailError = EmailValidator.validate(_email.text);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign up', style: theme.textTheme.headlineMedium),
+        backgroundColor: theme.primaryColor,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/images/Gemini_Generated_Image_exos46exos46exos.jpeg',
+                width: 200,
+                height: 200,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextField(
+                  controller: _email,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: 'Enter Email',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      errorText: _emailError),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
