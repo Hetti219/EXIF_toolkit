9b93aec7792e9c53962f73f9a20a0f32e7c85328
Hetti219 <sathikahettiarachchi219@gmail.com>
2024-07-17 22:42:24 +0530

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,6 +11,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(child: CupertinoNavigationBar(middle: 'Select an image',));
+    return CupertinoPageScaffold(child: CupertinoNavigationBar(middle: Text('Select an image'),));
   }
 }
