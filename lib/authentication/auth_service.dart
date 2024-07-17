065f8613222a74ba63880dea5f40f3163aed7254
Hetti219 <sathikahettiarachchi219@gmail.com>
2024-07-17 18:10:27 +0530

    Update auth_service.dart

--- lib/authentication/auth_service.dart
+++ lib/authentication/auth_service.dart
@@ -5,16 +5,28 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final auth = FirebaseAuth.instance;
 
-  FutureOr<User?> createUserWithEmailAndDeviceId(
-      String email, String deviceId) async {
+  FutureOr<User?> createUserWithEmailAndPassword(
+      String email, String password) async {
     try {
       final credentials = await auth.createUserWithEmailAndPassword(
-          email: email, password: deviceId);
+          email: email, password: password);
       return credentials.user;
     } catch (e) {
       log('Sign in error detected');
     }
 
+    return null;
+  }
+
+  FutureOr<User?> loginUserWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      final cred = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return cred.user;
+    } catch (e) {
+      log("Log In Error Detected");
+    }
     return null;
   }
 
