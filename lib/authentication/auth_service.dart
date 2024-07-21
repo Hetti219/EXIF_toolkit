6c9078757014098749c2d83e90910642c59d5258
Hetti219 <sathikahettiarachchi219@gmail.com>
2024-07-21 17:32:19 +0530

    Commented non working code.

--- lib/authentication/auth_service.dart
+++ lib/authentication/auth_service.dart
@@ -7,8 +7,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final _auth = FirebaseAuth.instance;
-  final _deviceId = DeviceDetails().getDeviceId();
-  final _databaseService = DatabaseService();
+
+  // final _deviceId = DeviceDetails().getDeviceId();
+  // final _databaseService = DatabaseService();
 
   Future<User?> createUserWithEmailAndPassword(
       String email, String password) async {
@@ -16,19 +17,19 @@ class AuthService {
       final credentials = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
 
-      final userId = credentials.user?.uid;
-      final deviceId = await _deviceId;
+      // final userId = credentials.user?.uid;
+      // final deviceId = await _deviceId;
 
-      if (userId != null && deviceId != null) {
-        final device = Device(userId: userId, deviceId: deviceId);
-        await _databaseService.addDevice(device);
-      } else {
-        throw Exception('Error: userId or deviceId is null during signup.');
-      }
+      // if (userId != null && deviceId != null) {
+      //   final device = Device(userId: userId, deviceId: deviceId);
+      //   await _databaseService.addDevice(device);
+      // } else {
+      //   throw Exception('Error: userId or deviceId is null during signup.');
+      // }
       return credentials.user;
-    } on FirebaseAuthException catch (e) {
-      log('Sign up error: ${e.code} - ${e.message}');
-      rethrow; // Rethrow for UI handling
+      // } on FirebaseAuthException catch (e) {
+      //   log('Sign up error: ${e.code} - ${e.message}');
+      //   rethrow;
     } catch (e) {
       log('Unexpected Sign Up Error: $e');
       rethrow;
@@ -40,28 +41,29 @@ class AuthService {
     try {
       final cred = await _auth.signInWithEmailAndPassword(
           email: email, password: password);
-      final userId = cred.user?.uid;
-      final deviceId = await _deviceId;
+      // final userId = cred.user?.uid;
+      // final deviceId = await _deviceId;
 
-      if (userId != null && deviceId != null) {
-        final isRegistered =
-            await _databaseService.isDeviceRegistered(userId, deviceId);
-        if (!isRegistered) {
-          await signOut(); // Sign the user out immediately
-          throw Exception('This device is not registered for this account.');
-        }
-        return cred.user;
-      } else {
-        log("Error: userId or deviceId is null during login.");
-        throw Exception('Login failed due to missing credentials.');
-      }
-    } on FirebaseAuthException catch (e) {
-      log('Log In Error: ${e.code} - ${e.message}');
-      rethrow;
+      // if (userId != null && deviceId != null) {
+      //   final isRegistered =
+      //       await _databaseService.isDeviceRegistered(userId, deviceId);
+      //   if (!isRegistered) {
+      //     await signOut(); // Sign the user out immediately
+      //     throw Exception('This device is not registered for this account.');
+      //   }
+      return cred.user;
+      // } else {
+      //   log("Error: userId or deviceId is null during login.");
+      //   throw Exception('Login failed due to missing credentials.');
+      // }
+      // } on FirebaseAuthException catch (e) {
+      //   log('Log In Error: ${e.code} - ${e.message}');
+      //   rethrow;
     } catch (e) {
       log("Log In Error Detected: $e");
-      rethrow;
+      // rethrow;
     }
+    return null;
   }
 
   FutureOr<void> signOut() async {
